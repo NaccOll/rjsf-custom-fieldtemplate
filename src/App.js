49a5933d188abfccea4e67939e369eefb803660b
NaccOll <wuyoubin0504@gmail.com>
2020-10-25 20:17:48 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Form } from 'antd'
+import "antd/dist/antd.css";
+import { withTheme } from '@rjsf/core'
+import { Theme as AntDTheme } from '@rjsf/antd'
+const JsonForm = withTheme(AntDTheme)
+class App extends React.Component {
+  componentDidMount() {
+    this.init()
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  init = () => {
+    new Promise((resolve, reject) => {
+      resolve(10)
+    }).then((data) => {
+      this.setState({ a: data })
+    })
+  }
+  render() {
+    return (
+      <JsonForm
+        className='ant-form ant-form-inline'
+        ObjectFieldTemplate={(props) => {
+          return (
+            <div className='ant-form ant-form-inline'>
+              {props.properties.map((element) => element.content)}
+            </div>
+          )
+        }}
+        FieldTemplate={(props) => {
+          const { label, required, children } = props
+          return (
+            <Form.Item label={label} required={required}>
+              {children}
+            </Form.Item>
+          )
+        }}
+        schema={{
+          type: 'object',
+          properties: {
+            firstName: { type: 'string', title: 'First Name' },
+            lastName: { type: 'string', title: 'Last Name' }
+          }
+        }}
+      />
+    )
+  }
 }
 
-export default App;
+export default App
